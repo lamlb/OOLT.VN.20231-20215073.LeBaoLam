58bd53c983144334c806cff5e1b3f2fed7f375b5
lamlb <lam.lb215073@sis.hust.edu.vn>
2023-10-28 17:55:02 +0700

    section 11,12,13

---
+++ Lab_02/Aimproject/src/Cart.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDER = 20;
+	
+	ArrayList<DigitalVideoDisc> itemsOrder = new ArrayList<DigitalVideoDisc>();
+	
+	private int qtyOrdered;
+	
+	public int getQtyOrder() {
+        return qtyOrdered;
+    }
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDER){
+            itemsOrder.add(disc);
+            qtyOrdered++;
+            System.out.printf("The disc %s has been added\n", disc.getTitle());
+        } else {
+            System.out.println("The cart is almost full");
+        }
+	}
+	
+	public void displayCart() {
+	    System.out.println("------- Cart Display -------");
+	    int index = 1;
+	    for (DigitalVideoDisc disc : itemsOrder){
+	        System.out.printf("%-4d %-20s %-5.2f \n", index, disc.getTitle(), disc.getCost());
+	        index++;
+	    }
+	}
+	
+	public double totalCost() {
+		double total = 0;
+		for (DigitalVideoDisc disc : itemsOrder){
+			total += (double) disc.getCost();
+		}
+		return total;
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered > 0) {
+			if (itemsOrder.contains(disc)) {
+				itemsOrder.remove(disc);
+				qtyOrdered--;
+				System.out.printf("The disc %s has been removed\n",disc.getTitle());
+			} else System.out.println("No found");
+		} else System.out.println("Empty cart");
+		
+	}
+}
