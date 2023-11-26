94bf5360541a253705867b1fbb1732a26fea3199
lamlb <lam.lb215073@sis.hust.edu.vn>
2023-11-26 21:52:37 +0700

    release

--- OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -1,5 +1,23 @@
 package hust.soict.hedspi.garbage;
 
-public class GarbageCreator {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class GarbageCreator {
+	public static void main(String[] args) throws IOException {
+		String filename = "//Users//the//Documents//Lab_OOP//Lab03//OtherProjects//src//hust//soict//hedspi//garbage//test.txt";
+		byte[] inputBytes = {0};
+		long startTime, endTime;
+		
+		inputBytes = Files.readAllBytes(Paths.get(filename));
+
+		startTime = System.currentTimeMillis();
+		String outputString = "";
+		for (byte b : inputBytes) {
+			outputString += (char)b;
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+	}
 }
